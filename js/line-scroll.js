26fb093cf86043f9497318aa3b1510a7fa19f645
zhengannyu <zhengannyu@gmail.com>
2021-10-23 02:03:52 +0800

    修改

---
+++ js/line-scroll.js
@@ -0,0 +1,12 @@
+//line1 = 字串.line-1
+// let line1 = document.querySelector('.display-title');
+let line2 = document.querySelector('.display-title');
+
+//當scroll
+window.onscroll = () => {
+  let pos = window.scrollY - 800;
+  //console.log(pos)
+
+  // line1.style.left = `${pos}px`
+  line2.style.right = `${pos}px`
+}

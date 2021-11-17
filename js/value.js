7f7620ec7782e7d1dcb0b7e2ac215de0809276c6
zhengannyu <zhengannyu@gmail.com>
2021-11-17 10:09:56 +0800

    更新work-img

---
+++ js/value.js
@@ -0,0 +1,14 @@
+function increaseValue() {
+  let value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  document.getElementById('number').value = value;
+}
+
+function decreaseValue() {
+  let value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value < 1 ? value = 1 : '';
+  value--;
+  document.getElementById('number').value = value;
+}
